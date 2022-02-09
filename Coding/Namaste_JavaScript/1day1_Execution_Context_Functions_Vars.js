6c598a6143f8175d53caa64beedaa756f763da93
Manasvi Sharma <manasvi.manu94@gmail.com>
2022-02-09 18:13:49 +0530

    Hoisting and Execution context Modified

--- Coding/Namaste_JavaScript/1day1_Execution_Context_Functions_Vars.js
+++ Coding/Namaste_JavaScript/1day1_Execution_Context_Functions_Vars.js
@@ -2,42 +2,42 @@
 
 //this also a good example to give while explaining HOISTING!
 
-// var x = 1;
-// a();
-// b();
-// console.log(x);
+var x = 1;
+a();
+b();
+console.log(x);
 
-// function a() {
-//   var x = 10;
-//   console.log(x);
-// }
+function a() {
+  var x = 10;
+  console.log(x);
+}
 
-// function b() {
-//   var x = 100;
-//   console.log(x);
-// }
+function b() {
+  var x = 100;
+  console.log(x);
+}
 
 //////////////////////////////////////////////
 
 //////////////////////////////>>>What happens when we declare a function with "var" insted of "function" keyword!
-var x = 1;
-a(); //TypeError: a is not a function.
-// As we are declaring function by saying it is a "var" in line 42 and we are trying to invoke a "var" here! As, in GEC it is assigned a value of undefined! and we are trying to invoke undefined!
-b();
+// var x = 1;
+// a(); //TypeError: a is not a function.
+// // As we are declaring function by saying it is a "var" in line 42 and we are trying to invoke a "var" here! As, in GEC it is assigned a value of undefined! and we are trying to invoke undefined!
+// b();
 
-console.log(x);
+// console.log(x);
 
-var a = () => {
-  const x = 10;
-  console.log(x);
-};
+// var a = () => {
+//   const x = 10;
+//   console.log(x);
+// };
 
-a();
+// a();
 
-function b() {
-  const x = 100;
-  console.log(x);
-}
+// function b() {
+//   const x = 100;
+//   console.log(x);
+// }
 
 ///////////////////////////////////////////////////////////
 
