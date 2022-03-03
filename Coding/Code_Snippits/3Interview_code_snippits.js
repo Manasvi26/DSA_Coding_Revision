4a2ece46f62bf835a9756a78a235a31ead7d3c6d
Manasvi Sharma <manasvi.manu94@gmail.com>
2022-03-03 10:05:41 +0530

    Event loop

---
+++ Coding/Code_Snippits/3Interview_code_snippits.js
@@ -0,0 +1,31 @@
+// let obj = {
+//   id: 1,
+// };
+
+// var obj2 = obj;
+
+// obj2.id = 2;
+
+// console.log(obj);
+// console.log(obj2);
+
+//flattening a object
+// var obj = {
+//   a: {
+//     b: {
+//       c: "test",
+//     },
+//     d: 2,
+//   },
+//   e: true,
+// };
+
+// ["test", 2, true]
+
+// console.log(obj.value);
+
+// Promise.resolve("first");
+// console.log("second");
+// setTimeout(() => {
+//   console.log("third");
+// });
--- Coding/Practice/2Event_Loop.js
+++ Coding/Practice/2Event_Loop.js
@@ -1,11 +1,38 @@
-console.log("Start of code");
+// console.log("Start of code");
 
-setTimeout(() => {
-  console.log("Inside SetTimeOut 1");
-});
+// setTimeout(() => {
+//   console.log("Inside SetTimeOut 1");
+// });
 
-setTimeout(() => {
-  console.log("Inside SetTimeOut 2");
-});
+// setTimeout(() => {
+//   console.log("Inside SetTimeOut 2");
+// });
 
-console.log("End of code");
+// console.log("End of code");
+
+for (var i = 0; i < 10; i++) {
+  setTimeout(() => {
+    console.log(i);
+  }, 2000);
+}
+
+var a = 333;
+console.log(a);
+
+for (let i = 0; i < 10; i++) {
+  setTimeout(() => {
+    console.log(i);
+  }, 2000);
+}
+
+for (var i = 0; i < 10; i++) {
+  setTimeout(() => {
+    console.log(i);
+  }, 2000 * i);
+}
+
+for (let i = 0; i < 10; i++) {
+  setTimeout(() => {
+    console.log(i);
+  }, 2000 * i);
+}
