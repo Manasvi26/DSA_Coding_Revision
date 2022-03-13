ede41298ca149be922408afcb371c0204edfb252
Manasvi Sharma <manasvi.manu94@gmail.com>
2022-03-13 13:27:21 +0530

    DSA prac.

---
+++ Coding/Practice/10SubString.js
@@ -0,0 +1,15 @@
+var str = "abc";
+
+var temp = [];
+function subString(str, k) {
+  for (let i = k; i < str.length; i++) {
+    temp.push(str[i]);
+
+    console.log(temp.join(" "));
+
+    subString(str, i + 1); //this is the tricky step
+    temp.pop();
+  }
+}
+
+subString(str, 0);
